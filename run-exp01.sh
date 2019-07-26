20fd8959124acecd5cfe9f2bc41f6b5daaa7e4b7
Carlos Pérez <cperez@dlsi.ua.es>
2019-07-26 13:40:20 +0200

    Fixed run-exp01.sh

--- run-exp01.sh
+++ run-exp01.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for TRAIN in 50 100 150 200 250 300 350 400
+for FOLD in 1 2 3 4 5
 do
-    for FOLD in 1 2 3 4 5
+    for TRAIN in 50 100 150 200 250 300 350 400
     do
         python code/experiment01.py --input-data data/B-59.850.lst --gpu 0 --train-limit $TRAIN --log exp01_train_${TRAIN}_${FOLD}-5
     done
+    python code/experiment01.py --input-data data/B-59.850.lst --gpu 0 --log exp01_train_FULL_${FOLD}-5
 done
